6408e529f358087e8bbc50ee4e5b6761d179fc9e
steaksauce1 <timt173@gmail.com>
2022-05-04 10:54:38 -0400

    CSS + More mastermind
    slow update on mastermind table layout. added laravel-based css back into the layout file

--- resources/views/mastermind/mastermind.blade.php
+++ resources/views/mastermind/mastermind.blade.php
@@ -1,32 +1,118 @@
 <x-layout>
 	<h1>M A S T E R M I N D</h1>
-	<style type="text/css">
-		table, th, td {
-  border: 1px solid;
-}
-	</style>
-	<table style="text-align: center; align-content: center; ">
+
+	<div class="place-content-center">
+	<table class="table-auto border-collapse border border-slate-500 ">
 		<tr>
-			<th>1</th>
-			<th>2</th>
-			<th>3</th>
-			<th>4</th>
+			<th class="border border-slate-600">Column 1</th>
+			<th class="border border-slate-600">Column 2</th>
+			<th class="border border-slate-600">Column 3</th>
+			<th class="border border-slate-600">Column 4</th>
+			<th class="border border-slate-600">Correct Colors</th>
+			<th class="border border-slate-600">Correct Placement</th>
 		</tr>
 		<tr>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
 		</tr>
 		<tr>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td>4</td>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
 		</tr>
-
-		@include('mastermind._color-select')
-
-
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
+		<tr>
+			<td class="border border-slate-700">1</td>
+			<td class="border border-slate-700">2</td>
+			<td class="border border-slate-700">3</td>
+			<td class="border border-slate-700">4</td>
+			<td class="border border-slate-700">...</td>
+			<td class="border border-slate-700">...</td>
+		</tr>
 	</table>
+	</div>
+	<div><button style="color: black;">Submit </button><br></div>
+<div class="grid grid-cols-4 gap-4">
+	<div>
+		@include('mastermind._color-select')
+	</div>
+
+	<div>
+		@include('mastermind._color-select')
+	</div>
+
+	<div>
+		@include('mastermind._color-select')
+	</div>
+
+	<div>
+		@include('mastermind._color-select')
+	</div>
+</div>
+	
+
+<br>
+<button style="color: black;">Black </button><br>
 </x-layout>
